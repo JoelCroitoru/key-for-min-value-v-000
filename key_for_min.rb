886f0241b9c8c08711a23dfc81c03f77c1c88217
Joel Croitoru <jcroitoru@gmail.com>
2020-07-31 15:19:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,10 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_value_key = nil
   name_hash.each do |key, value|
-    sum = value
+    min_value = value
     binding.pry
-    if sum < value
-
-      return sum
+    if min_value == nil || min_value > value 
+      min_value_key = key
     elsif value < sum
       min_value_key = key
   #  else
