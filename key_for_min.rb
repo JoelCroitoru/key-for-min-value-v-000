5830b58b9d617ccf383849203f5f2787602706e3
Joel Croitoru <jcroitoru@gmail.com>
2020-07-31 14:41:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  array = []
   name_hash.each do |key, value|
-    value.sort
-
+    array << value.sort 
   end
+  array[0]
 end
